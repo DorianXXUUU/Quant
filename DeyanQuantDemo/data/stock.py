b97e7a789818e00b2d4da754c6f53e28046a37a7
许德琰 <xudeyan@xudeyandeMacBook-Pro.local>
2022-02-23 16:18:36 +0800

    行情记录：获取股票列表、行情数据、基本面指标、周期转换

--- DeyanQuantDemo/data/stock.py
+++ DeyanQuantDemo/data/stock.py
@@ -10,69 +10,83 @@ pd.set_option('display.max_row', 100000)
 pd.set_option('display.max_columns', 1000)
 
 auth('18888681090', 'Xudeyan0939')  # 账号是申请时所填写的手机号；密码为聚宽官网登录密码
-#
-# # Shanghai .XSHG
-# # Shenzhen .XSHE
-#
-# # print(df)
-# # 获取所有A股
-#
-# # stocks = list(get_all_securities(['stock']).index)
-# # # print(stocks)
-# #
-# # df = get_price(stocks, end_date='2022-02-18',
-# #                count=10,
-# #                frequency='daily',
-# #                fields=['open', 'close', 'high', 'low', 'volume', 'money'])
-# # print(df)
-#
-# # resample函数的使用
-# # 转换周期：日K to 周K
-# # 获取日K
-# df = get_price('000001.XSHG', end_date='2021-02-22',
-#                count=20,
-#                frequency='daily',
-#                fields=['open', 'close', 'high', 'low', 'volume', 'money'])
-#
-# df['weekday'] = df.index.weekday
-# print(df)
-# # 获取周K：当周的开盘价 收盘价 最高价 最低价
-# df_week = pd.DataFrame()
-# df_week['open'] = df['open'].resample('W').first()
-# df_week['close'] = df['close'].resample('W').last()
-# df_week['high'] = df['high'].resample('W').max()
-# df_week['low'] = df['low'].resample('W').min()
-#
-# # 汇总统计：统计一下月成交量、成交额（sum）
-# df_week['volume(sum)'] = df['volume'].resample('W').sum()
-# df_week['money(sum)'] = df['money'].resample('W').sum()
 
-# print(df_week)
 
-'''获取股票财务指标'''
-df = get_fundamentals(query(indicator), statDate='2020')
-# print(df)
-# 存储数据
-# df.to_csv('/Users/xudeyan/desktop/Quant/DeyanQuantDemo/data/finance/finance2020.csv')
+def get_stock_list():
+    """
+    获取所有A股股票列表
+    :return stock_list
+    """
+    stocks = list(get_all_securities(['stock']).index)
+    return stocks
 
-# 基于盈利指标选股：e ps，operating_profit, roe, inc_net_profit_year_on_year
-#
-# df = df[(df['eps'] > 0) &
-#         (df['operating_profit'] > 3000000000) &
-#         (df['roe'] > 11) &
-#         (df['inc_net_profit_year_on_year'] > 10)]
-df = df[(df['eps'] > 0) &
-        (df['roe'] > 11) &
-        (df['inc_net_profit_year_on_year'] > 10)]
 
-df.index = df['code']
-# print(df.head())
+def get_single_price(code, time_frequency, start_date, end_date):
+    """
+    获取单个股票行情数据
+    :param code:
+    :param time_frequency:
+    :param start_date:
+    :param end_date:
+    :return:
+    """
+    data = get_price(code,
+                     start_date=start_date,
+                     end_date=end_date,
+                     frequency=time_frequency)
+    return data
 
-'''获取股票估值指标'''
-df_valuation = get_fundamentals(query(valuation), date='2021-2-22')
-df_valuation.index = df_valuation['code']
 
-df['pe_ratio'] = df_valuation['pe_ratio']
-df = df[df['pe_ratio'] < 50]
-# print(df_valuation.head())
-print(df.head(n=10))
+def export_date(data, filename, type):
+    """
+    导出股票行情数据
+    :param data:
+    :param filename:
+    :param type: price, finance
+    :return:
+    """
+    file_root = '/Users/xudeyan/Desktop/Quant/DeyanQuantDemo/data/' + type + '/' + filename + '.csv '
+    data.to_csv(file_root)
+    print("Saved Successful")
+
+
+def transfer_price_freq(data, time_freq):
+    """
+    转换股票行情周期
+    :param data:
+    :param time_freq:
+    :return:
+    """
+    df_trans = pd.DataFrame()
+    df_trans['open'] = data['open'].resample(time_freq).first()
+    df_trans['close'] = data['close'].resample(time_freq).last()
+    df_trans['high'] = data['high'].resample(time_freq).max()
+    df_trans['low'] = data['low'].resample(time_freq).min()
+
+    return df_trans
+
+
+def get_single_finance(code, date, statDate):
+    """
+    获取单个股票财务指标
+    :param code:
+    :param date:
+    :param statDate:
+    :return:
+    """
+    data = get_fundamentals(query(indicator).filter(indicator.code == code), date=date, statDate=statDate)
+    return data
+
+
+def get_single_valuation(code, date, statDate):
+    """
+    获取单个股票估值指标
+    :param code:
+    :param date:
+    :param statDate:
+    :return:
+    """
+    data = get_fundamentals(query(valuation).filter(valuation.code == code), date=date, statDate=statDate)
+    return data
+
+
